4aa394f2203e891455d5555d96ec709b267fead6
Dave Niewinski <davesarmoury@gmail.com>
2023-12-08 21:01:50 -0500

    Added in the chat endpoint and a basic test to see whether the model selected is a legacy model that uses the completion endpoint or the chat endpoint

--- scripts/openai_node.py
+++ scripts/openai_node.py
@@ -5,7 +5,7 @@ from openai_ros.srv import Completion, CompletionResponse
 import rospy
 from openai import OpenAI
 
-def servicer(req):
+def legacy_servicer(req):
     global client, max_tokens, model
     res = CompletionResponse()
 
@@ -13,6 +13,24 @@ def servicer(req):
 
     res.finish_reason = response.choices[0].finish_reason
     res.text = response.choices[0].text
+    res.model = response.model
+    res.completion_tokens = response.usage.completion_tokens
+    res.prompt_tokens = response.usage.prompt_tokens
+    res.total_tokens = response.usage.total_tokens
+
+    # When response is not working, completion_tokens is None, which chase error on CompleteResponse format(int32)
+    if not isinstance(res.completion_tokens, int):
+        res.completion_tokens = -1
+    return res
+
+def chat_servicer(req):
+    global client, max_tokens, model
+    res = CompletionResponse()
+
+    response = client.chat.completions.create(model=model, messages=[{"role": "user", "content": req.prompt}], temperature=req.temperature, max_tokens=max_tokens)
+
+    res.finish_reason = response.choices[0].finish_reason
+    res.text = response.choices[0].message.content
     res.model = response.model
     res.completion_tokens = response.usage.completion_tokens
     res.prompt_tokens = response.usage.prompt_tokens
@@ -37,13 +55,25 @@ def main():
         models_msg.data.append(m.id)
 
     if model not in models_msg.data:
-        rospy.logwarn(model + " is not an available model")
-        rospy.logwarn(models_msg.data)
+        rospy.logerr(model + " is not an available model")
+        rospy.logerr(models_msg.data)
         return
 
     pub.publish(models_msg)
 
-    rospy.Service('get_response', Completion, servicer)
+    endpoint = "chat"
+    try:
+        client.chat.completions.create(model=model, messages=[{"role": "user", "content": "ignore this message"}], max_tokens=1)
+    except:
+        endpoint = "legacy"
+
+    rospy.loginfo("Using " + model + " though " + endpoint + " endpoint")
+
+    if endpoint == "legacy":
+        rospy.Service('get_response', Completion, legacy_servicer)
+    else:
+        rospy.Service('get_response', Completion, chat_servicer)
+
     rospy.spin()
 
 if __name__ == '__main__':
